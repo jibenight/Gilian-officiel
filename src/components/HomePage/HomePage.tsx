1bcb427e929b7f907f27edd75d923d3236468449
Jean <nguyen.jean@me.com>
2023-06-06 08:45:30 +0200

    add components

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+import './HomePage.css';
+import Menu from '../Menu/Menu';
+
+function HomePage() {
+  return (
+    <>
+      <div>
+        <Menu />
+        <h1>Gilian</h1>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,26 @@
+import './Menu.css';
+
+function Menu() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href='/ep'>Ep</a>
+          </li>
+          <li>
+            <a href='/shooting'>Shooting</a>
+          </li>
+          <li>
+            <a href='/me'>Me</a>
+          </li>
+          <li>
+            <a href='/contact'>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Menu;
